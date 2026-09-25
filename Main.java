28d5dd9fa86c80652fbcb10c8c09f25b11d9c7d4
Muhammad Faiq Khan <m.faiq.khan78@gmail.com>
2026-09-25 22:00:09 +0500

    Fix typo in output message

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins! The Java pipeline is working.");
+        System.out.println("Hello, Jenkins! The Java pipeline is working..");
     }
 }
